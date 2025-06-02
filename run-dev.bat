84f671cc3a3a75049472cd89201aecafebacf726
ZenMachina16 <binayakb1604@gmail.com>
2025-06-02 15:46:18 +0530

    Removed unused files

--- run-dev.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting URL Shortener Development Environment...
-
-start cmd /k "echo Starting Go Backend... && go run main.go"
-start cmd /k "echo Starting Next.js Frontend... && cd frontend && npm run dev"
- 
-echo Development environment started:
-echo - Backend: http://localhost:9808
-echo - Frontend: http://localhost:3000 
